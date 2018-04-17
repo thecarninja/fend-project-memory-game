2be9adf0b9e54b5f5193a3ea81491211df985e3d
Steve Mahlstedt <steve.mahlstedt@gmail.com>
2018-04-17 10:11:48 -0400

    Added deck array with all cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const deck = ["fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", 
+			"fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", 
+			"fa fa-bicycle", "fa fa-bicycle", "fa fa-diamond", "fa fa-diamond", 
+			"fa fa-bomb", "fa fa-bomb", "fa fa-paper-plane-o", 
+			"fa fa-paper-plane-o"]
 
 /*
  * Display the cards on the page
@@ -36,3 +40,5 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+displayCards();
